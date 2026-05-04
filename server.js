c688eca85ff359d1c526a7b372f1c415d3da632d
Pontus Johansson <johanssonp998@gmail.com>
2026-05-04 12:58:41 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+// Ladda variabler från .env
+require("dotenv").config();
+
+// Importerar moduler
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+// Skapar expressapplikation
+const app = express();
+
+// Tillåter CORS
+app.use(cors());
+app.use(express.json());
+
+// Ansluter till MongoDB via connection variabler i .env
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => console.log("Ansluten till MongoDB"))
+    .catch(err => console.error("MongoDB-anslutningen misslyckades: ", err));
+
+
+// Test-route
+app.get("/", (req, res) => {
+    res.send("API igång.");
+});
+// Startar servern på den port som anges i env, med port 3000 som fallbak
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servern ansluten på port: ${PORT}`));
