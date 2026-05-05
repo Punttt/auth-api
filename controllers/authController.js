4fba96fcd21c67ded1751e6f093e1be13b983154
Pontus Johansson <johanssonp998@gmail.com>
2026-05-05 13:46:15 +0200

    skriver klart registrering och validering för registrering.

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,32 @@ const bcrypt = require("bcrypt");
 // Registrerar ny användare
 const register = async (req, res) => {
     try {
+        const { username, email, password } = req.body;
+
+        // Kontrollerar att alla fält finns
+        if ( !username || !email || !password ){
+            return res.status(400).json({ error: "Alla fält krävs" });
+        }
+
+        // Hashar lösenordet
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Skapar och spara en användare
+        const newUser = await User.create({
+            username,
+            email,
+            password: hashedPassword
+        });
+
+        // Skrier ut bekräftelse, skriver ut följande: ID, Username, Email
+        res.status(201).json({
+            message: "Användaren är skapad",
+            user: {
+                id: newUser._id,
+                username: newUser.username,
+                email: newUser.email
+            }
+        });
 
     } catch (error){
         console.error(error);
