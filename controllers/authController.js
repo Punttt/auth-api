7ca63f35717b227e588959cbb4e03c41d103de63
Pontus Johansson <johanssonp998@gmail.com>
2026-05-05 13:35:33 +0200

    påbörjar med controllers / validering för fält

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,2 +1,12 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+
+// Registrerar ny användare
+const register = async (req, res) => {
+    try {
+
+    } catch (error){
+        console.error(error);
+        res.status(400).json({ error: "Kunde inte skapa ett nytt konto" })
+    }
+};
