67d377a2588b5a05e5e9d18b3e062bf5e6e9ba6b
Pontus Johansson <johanssonp998@gmail.com>
2026-05-06 14:26:45 +0200

    påbörjar get route med try och catch

--- routes/protectedRoutes.js
+++ routes/protectedRoutes.js
@@ -2,3 +2,13 @@ const express = require("express");
 const router = express.Router();
 const authenticate = require("../middleware/authMiddleware");
 const User = require("../models/User");
+
+// GET /api/profile - måste ha giltig JWT
+router.get("/profile", authenticate, async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internt serverfel" });
+    }
+});
