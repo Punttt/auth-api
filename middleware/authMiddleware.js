d1d26a3b7e5b750576d3c6360108d4f8624438bb
Pontus Johansson <johanssonp998@gmail.com>
2026-05-06 14:49:31 +0200

    fixar ett stavfel i middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 // Middleware som verifierar JWT i headern
 const authenticate = (req, res, next) => {
-    const authHeader = req.header.authorization;
+    const authHeader = req.headers.authorization;
 
     if( !authHeader || !authHeader.startsWith("Bearer ")){
         return res.status(401).json({ error: "Ingen giltig token skickades" });
