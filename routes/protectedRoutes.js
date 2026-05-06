f93f5fae2d35f4acbb86a404b6759181bbc1383f
Pontus Johansson <johanssonp998@gmail.com>
2026-05-06 14:31:54 +0200

    kopplar samman och hämtar req.user från middleware samt validerar

--- routes/protectedRoutes.js
+++ routes/protectedRoutes.js
@@ -6,7 +6,18 @@ const User = require("../models/User");
 // GET /api/profile - måste ha giltig JWT
 router.get("/profile", authenticate, async (req, res) => {
     try {
+        // hämtar user fråpn middleware coh innehåller userId från token
+        const user = await User.findById(req.user.userId).select("-password");
 
+        if(!user) {
+            return res.status(404).json({ error: "Användaren hittades inte" });
+        }
+
+        res.status(200).json({
+            message: "Här är din profil",
+            user
+        });
+        
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Internt serverfel" });
